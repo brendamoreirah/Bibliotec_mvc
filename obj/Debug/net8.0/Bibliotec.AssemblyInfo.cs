f3a0f03aef112fb69e148086352d175b06e0e7ac
Brenda Mariano Moreira <brenda.m.moreira6@aluno.senai.br>
2024-11-26 14:57:23 -0300

    atualizando bibliotec/index

--- obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+553e211c59db7413d0c1cc13380ddbfb54ed4ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20da865cbe6c87f58de25deae03d3a49141553e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
